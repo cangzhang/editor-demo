830ea423174d06bb50611568ed9fe6588a7d5d6f
al <cangzan@gmail.com>
2022-04-25 17:21:18 +0800

    doc: readme.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Plain Markdown Example for Lexical editor
+
+## Start
+
+```console
+pnpm install
+pnpm run start
+```
+
+Open [http://localhost:1234](http://localhost:1234/) in browser.
--- src/SuggestionList.js
+++ src/SuggestionList.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 export function SuggestionList({
